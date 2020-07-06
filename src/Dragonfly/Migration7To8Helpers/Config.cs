8c52e740627084c1ffdfb884d604a18102a12ceb
Heather Floyd <heather@floydinnovations.com>
2020-07-06 11:32:50 -0400

    WebApi

---
+++ src/Dragonfly/Migration7To8Helpers/Config.cs
@@ -0,0 +1,96 @@
+﻿namespace Dragonfly.Migration7To8Helpers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Web.Hosting;
+    using System.Xml.Serialization;
+
+    ///// <summary>
+    ///// Configuration settings read from a config XML file
+    ///// </summary>
+    //[XmlRoot("Dragonfly")]
+    public class Config
+    {
+        private const string ConfigFileName = "Dragonfly.config";
+        private const string AppPluginsPath = "/App_Plugins/Dragonfly.Migration7To8Helpers/";
+        private const string DefaultDataPath = "/App_Data/Dragonfly.Migration7To8Helpers/";
+        private static Config _config;
+
+        public DateTime ConfigTimestamp { get; set; }
+
+        //[XmlElement]
+        //public string LocalTimezone { get; set; }
+
+        //[XmlArray("MultipleThings")]
+        //[XmlArrayItem("Thing")]
+        //public List<string> MultipleThings = new List<string>();
+
+        public static Config GetConfig()
+        {
+            _config = new Config();
+
+            ////Read XML Config
+            //string path = $"~/Config/{ConfigFileName}";
+            //string pathMapped = HostingEnvironment.MapPath(path);
+            //var lastModified = System.IO.File.GetLastWriteTime(pathMapped);
+
+            //if (_config == null || lastModified > _config.ConfigTimestamp)
+            //{
+            //    XmlSerializer serializer = new XmlSerializer(typeof(Config));
+
+            //    // Read from file
+            //    try
+            //    {
+            //        using (var reader = new StreamReader(pathMapped))
+            //        {
+            //            _config = (Config)serializer.Deserialize(reader);
+            //        }
+            //    }
+            //    catch (InvalidOperationException ex)
+            //    {
+            //        throw new InvalidOperationException($"The format of '{ConfigFileName}' is invalid. Details: " + ex.Message, ex.InnerException);
+            //    }
+
+            //    _config.ConfigTimestamp = lastModified;
+            //}
+
+            return _config;
+        }
+
+        public string GetAppPluginsPath(bool ReturnMapped = false)
+        {
+            var path = ConfigFileName;
+
+            if (ReturnMapped)
+            {
+                return HostingEnvironment.MapPath(path);
+            }
+            else
+            {
+                return path;
+            }
+        }
+
+        public string GetViewsPath()
+        {
+            var path = GetAppPluginsPath() + "Views/";
+            return path;
+        }
+
+        public string GetDataPath(bool ReturnMapped = false)
+        {
+            var path = DefaultDataPath;
+            //var path = _config.DataStoragePath != "" ? _config.DataStoragePath : DefaultDataPath;
+
+            if (ReturnMapped)
+            {
+                return HostingEnvironment.MapPath(path);
+            }
+            else
+            {
+                return path;
+            }
+        }
+    }
+}
---
+++ src/Dragonfly/Migration7To8Helpers/MigrationHelperApiController.cs
@@ -0,0 +1,93 @@
+﻿namespace Dragonfly.Migration7To8Helpers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Net.Http;
+    using System.Text;
+    using System.Web;
+    using System.Web.Mvc;
+    using Dragonfly.NetHelpers;
+    using Dragonfly.NetModels;
+    using Dragonfly.UmbracoHelpers;
+    using Newtonsoft.Json;
+    using Umbraco.Web.WebApi;
+
+    // [IsBackOffice]
+    // GET: /Umbraco/backoffice/Api/MigrationHelperApi <-- UmbracoAuthorizedApiController
+
+    [IsBackOffice]
+    class MigrationHelperApiController : UmbracoAuthorizedApiController
+    {
+        private Config _Config = Config.GetConfig();
+
+        /// /Umbraco/backoffice/Api/MigrationHelperApi/Start
+        [System.Web.Http.AcceptVerbs("GET")]
+        public HttpResponseMessage Start()
+        {
+            var returnSB = new StringBuilder();
+            var returnStatusMsg = new StatusMessage(true); //assume success
+            var pvPath = $"{_Config.GetViewsPath()}Start.cshtml";
+
+            //Setup
+         
+
+            //GET DATA TO DISPLAY
+
+            //UPDATE STATUS MSG
+            //returnStatusMsg.Success = true;
+
+
+            //VIEW DATA 
+            var viewData = new ViewDataDictionary();
+            viewData.Model = returnStatusMsg;
+            viewData.Add("SpecialMessage", "");
+            viewData.Add("SpecialMessageClass", "bg-info");
+
+            //RENDER
+            var controllerContext = this.ControllerContext;
+            var displayHtml =
+                ApiControllerHtmlHelper.GetPartialViewHtml(controllerContext, pvPath, viewData, HttpContext.Current);
+            returnSB.AppendLine(displayHtml);
+
+            //RETURN AS HTML
+            return new HttpResponseMessage()
+            {
+                Content = new StringContent(
+                    returnSB.ToString(),
+                    Encoding.UTF8,
+                    "text/html"
+                )
+            };
+        }
+
+        /// /Umbraco/backoffice/Api/MigrationHelperApi/TestRaw
+        [System.Web.Http.AcceptVerbs("GET")]
+        public HttpResponseMessage TestRaw()
+        {
+            var returnSB = new StringBuilder();
+            var returnStatus = new StatusMessage();
+
+            //GET DATA TO DISPLAY
+
+            //UPDATE STATUS MSG
+
+
+            string json = JsonConvert.SerializeObject(returnStatus);
+
+            returnSB.AppendLine(json);
+
+            return new HttpResponseMessage()
+            {
+                Content = new StringContent(
+                    returnSB.ToString(),
+                    Encoding.UTF8,
+                    "application/json"
+                )
+            };
+        }
+
+
+
+    }
+}
